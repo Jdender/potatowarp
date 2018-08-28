edce176b08dd0b70bb6f5ee32191b0b2befcd99c
James <jdenderplays@gmail.com>
2018-08-28 19:26:27 -0400

    Add the rest of User.enti

--- src/user/User.enti.ts
+++ src/user/User.enti.ts
@@ -1,7 +1,10 @@
-import { Entity, PrimaryColumn, Column, BeforeInsert } from 'typeorm';
+import { Entity, PrimaryColumn, Column, BeforeInsert, BeforeUpdate } from 'typeorm';
 import FlakeId from 'flakeid';
+import { hash } from 'bcrypt';
 
 const userFlake = new FlakeId();
+
+const saltRounds = 10;
 
 @Entity()
 export class User {
@@ -15,5 +18,24 @@ export class User {
     }
 
     @Column()
-    name: string;
+    username: string;
+
+    @Column()
+    displayName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    private async hashPassword() {
+        // If password is raw than hash it
+        this.password = this.password.startsWith('=raw=')
+        ? await hash(this.password, saltRounds)
+        : this.password;
+    }
+
 }
