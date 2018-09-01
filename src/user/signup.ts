ca69571c484081d0f2fabf38d3a72b3f9d13b729
James <jdenderplays@gmail.com>
2018-08-31 21:48:54 -0400

    Add postSignup to signup.ctrl

--- src/user/signup.ts
+++ src/user/signup.ts
@@ -1,11 +1,30 @@
-import { Controller, Get, Render, Post } from 'routing-controllers';
+import { Controller, Get, Render, Post, UseBefore } from 'routing-controllers';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+import { EntityFromBody } from 'typeorm-routing-controllers-extensions';
+import { User } from './User.enti';
+import { urlencoded } from 'body-parser';
 
 @Controller()
 export class SignupCtrl {
+
+    @InjectRepository(User)
+    private users: Repository<User>;
 
     @Get('/signup')
     @Render('user/signup')
     getSignup() {
         return {};
+    }
+
+    @Post('/signup')
+    @UseBefore(urlencoded({ extended: false }))
+    postSignup(
+        @EntityFromBody({ required: true }) user: User
+    ) {
+        // Mark as raw so it gets hashed
+        user.password = '=raw='.concat(user.password);
+
+        return this.users.save(user);
     }
 }
