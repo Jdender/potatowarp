b101588ee421a5c39d4fd607f2545ba913fae3cf
James <jdenderplays@gmail.com>
2018-09-04 19:12:01 -0400

    Provide user object to template in threads.ctrl

--- src/thread/threads.ctrl.ts
+++ src/thread/threads.ctrl.ts
@@ -1,11 +1,22 @@
-import { Controller, Get, Render } from 'routing-controllers';
+import { Controller, Get, Render, Session } from 'routing-controllers';
+import { InjectRepository } from 'typeorm-typedi-extensions';
+import { User } from '../user/User.enti';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class ThreadCtrl {
 
+    @InjectRepository(User)
+    private users: Repository<User>;
+
     @Get('/')
     @Render('thread/main')
-    getMain() {
-        return {};
+    async getMain(
+        @Session('userId', { required: false }) id: string,
+    ) {
+        console.log(id)
+        const user = await this.users.findOne({ where: { id } });
+
+        return { user };
     }
 }
