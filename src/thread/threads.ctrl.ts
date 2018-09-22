e08c3633f1b3286e5443571722ee593d23b7622b
jdenderplaysipconfig <jdenderplays@gmail.com>
2018-09-22 14:01:40 -0400

    Add getThreadById to threads.ctrl

--- src/thread/threads.ctrl.ts
+++ src/thread/threads.ctrl.ts
@@ -1,21 +1,36 @@
-import { Controller, Get, Render, Session } from 'routing-controllers';
+import { Controller, Get, Render, CurrentUser, NotFoundError, Param } from 'routing-controllers';
 import { InjectRepository } from 'typeorm-typedi-extensions';
 import { User } from '../user/User.enti';
 import { Repository } from 'typeorm';
+import { Thread } from './Thread.enti';
 
 @Controller()
 export class ThreadCtrl {
 
-    @InjectRepository(User)
-    private users: Repository<User>;
+    @InjectRepository(Thread)
+    private threads: Repository<Thread>;
 
     @Get('/')
     @Render('thread/main')
     async getMain(
-        @Session('userId', { required: false }) id: string,
+        @CurrentUser() user: User,
     ) {
-        const user = await this.users.findOne({ where: { id } });
+        const threads = await this.threads.find();
 
-        return { user };
+        return { user, threads };
+    }
+
+    @Get('/thread/:id')
+    @Render('thread/thread')
+    async getThread(
+        @CurrentUser() user: User,
+        //@EntityFromParam('id', { required: true }) thread: Thread,
+        @Param('id') id: string,
+    ) {
+        const thread = await this.threads.findOne(id);
+
+        if (!thread) throw new NotFoundError('Thread not found');
+
+        return { user, thread };
     }
 }
